773614a0f3b6bbe7e4f3154e4f10d44cd30eda4b
Jeison Ferrer <jeison120120@hotmail.com>
2023-03-21 10:53:35 -0500

    feact: Estructura basica del proyecto 1. Estructura para las pages de administación. 2. Estructura para las pages de commerce.

---
+++ src/procenterapp/layout/ProcenterAppLayout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProcenterAppLayout = () => {
+  return (
+    <div>ProcenterAppLayout</div>
+  )
+}
---
+++ src/procenterapp/pages/Catalogo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Catalogo = () => {
+  return (
+    <div>Catalogo</div>
+  )
+}
