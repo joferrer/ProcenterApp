22de98c65cb0d7919d6fec209684231441d09860
GUILLERMOGU24 <jguillermogu24@gmail.com>
2023-03-31 12:15:52 -0500

    Rutas creadas

---
+++ src/procenterapp/pages/Administrador.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Administrador = () => {
+  return <div>Administrador</div>;
+};
--- src/procenterapp/routes/ProcenterAppRouter.jsx
+++ src/procenterapp/routes/ProcenterAppRouter.jsx
@@ -1,11 +1,21 @@
 import { Home } from "../pages/Home";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Catalogo } from "../pages/Catalogo";
+import { Simulador } from "./../pages/Simulador";
+import { Historial } from "./../pages/Historial";
+import { Asesores } from "./../pages/Asesores";
+import { Administrador } from "./../pages/Administrador";
 
 export const ProcenterAppRouter = () => {
   return (
     <Routes>
       <Route path="/proapp/home" element={<Home />}></Route>
+      <Route path="/proapp/catalogo" element={<Catalogo />}></Route>
+      <Route path="/proapp/simulador" element={<Simulador />}></Route>
+      <Route path="/proapp/historial" element={<Historial />}></Route>
+      <Route path="/proapp/asesores" element={<Asesores />}></Route>
+      <Route path="/proapp/administrar" element={<Administrador />}></Route>
     </Routes>
   );
 };
