816d95dd73e00700d290a9321bce0d7de676fdf9
GUILLERMOGU24 <jguillermogu24@gmail.com>
2023-05-29 16:51:33 -0500

    inicio de layout de ecomerce

--- src/procentercommerce/layout/ProcenterCommerceLayout.jsx
+++ src/procentercommerce/layout/ProcenterCommerceLayout.jsx
@@ -1,7 +1,132 @@
-import React from 'react'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 
-export const ProcenterCommerceLayout = () => {
+const pages = ["Products", "Pricing", "Blog"];
+
+function ProcenterCommerceLayout({ children }) {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <div>ProcenterCommerceLayout</div>
-  )
+    <Box sx={{ mb: 6 }}>
+      <Box sx={{ width: "100%", height: "100%", mb: 10 }}>
+        <AppBar sx={{ backgroundColor: "black", width: "100%" }}>
+          <Container sx={{ width: "100%" }}>
+            <Toolbar sx={{ width: "100%", display: "flex" }}>
+              <Box
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                <img src="../../../public/Images/LUCKY5.png" id="img6" />
+              </Box>
+
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+
+              <Box
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                  justifyContent: "center",
+                }}
+              >
+                <img src="../../../public/Images/LUCKY5.png" id="img6" />
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  "@media (max-width:899px)": {
+                    display: "none",
+                  },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
+      <Box sx={{ m: 3, height: "100%" }}>{children}</Box>
+      <Box>
+        <Box></Box>
+        <Box
+          sx={{ width: "100%", height: "30px", backgroundColor: "black" }}
+        ></Box>
+        <Box
+          sx={{ width: "100%", height: "40px", backgroundColor: "grey" }}
+        ></Box>
+      </Box>
+    </Box>
+  );
 }
+export default ProcenterCommerceLayout;
