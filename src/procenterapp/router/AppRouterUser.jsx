9351b7f0f73fbb09ec29a380a84dc4934fe1a81f
Jeison Ferrer <jeison120120@hotmail.com>
2023-05-28 22:18:55 -0500

    feact: enruando ando

--- src/procenterapp/router/AppRouterUser.jsx
+++ src/procenterapp/router/AppRouterUser.jsx
@@ -1,33 +1,40 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-
-//import { useCheckAuth } from '../hooks';
-
-//import { CheckingAuth } from '../ui';
 import { AdminRouter } from './../VistaAdmin/routes/AdminRouter';
 import { EmpleadoRouter } from "../VistaEmpleado/routes/EmpleadoRouter";
 import { PublicistaRouter } from "../VistaPublicista";
+import { authDispatch } from "../../store/auth/authDispatch";
+
+const ROL_ASESOR        = "ASESOR"
+const ROL_CLIENTE       = "CLIENTE"
+const ROL_PUBLICISTA    = "PUBLICISTA"
+const ROL_ADMINISTRADOR = "ADMINISTRADOR"
+
 
 export const AppRouterUser = () => {
-  //const {status} = useCheckAuth();
+  const {rol} = authDispatch();
 
-  // if(status === 'checking'){
-  // return <CheckingAuth />
-  //}
-
-  /*
-  {status === "authenticated" ? (
-        <Route path="/*" element={<ProcenterAppRouter />} />
-      ) : (
-        <Route path="/auth/*" element={<AuthRouter />} />
-      )}
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
-  */
-  //const status = "authenticated";
+  if(rol === ROL_ADMINISTRADOR){
+    return <Routes>
+     
+    </Routes>
+  }
   return (
     <Routes>
-      <Route path="/proapp/*" element={<AdminRouter />}></Route>
-      <Route path="/asesor/*" element={<EmpleadoRouter />}></Route>
-      <Route path="/publicista/*" element={<PublicistaRouter />}></Route>
+      {
+        rol === ROL_ASESOR  ? 
+        <Route path="/asesor/*" element={<EmpleadoRouter />}></Route>
+        :
+        rol === ROL_PUBLICISTA ? 
+        <Route path="/publicista/*" element={<PublicistaRouter />}></Route>
+        :
+        rol === ROL_ADMINISTRADOR ? 
+        <Route path="/proapp/*" element={<AdminRouter />}></Route>
+        :
+        <Route path="/*" element={<Navigate to="/auth/login" />} />
+      }
+      
+      
+      
     </Routes>
   );
 };
