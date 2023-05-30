c29fd79879338ffee31248a2e3dd06e0f460366b
GUILLERMOGU24 <jguillermogu24@gmail.com>
2023-05-30 17:39:57 -0500

    Comienzo de ecommer

--- src/procentercommerce/pages/InformacionEmpresarial.jsx
+++ src/procentercommerce/pages/InformacionEmpresarial.jsx
@@ -1,11 +1,37 @@
-import { Typography } from "@mui/material"
-import ProcenterCommerceLayout from "../layout/ProcenterCommerceLayout"
-
+import { Box, Grid, Typography } from "@mui/material";
+import ProcenterCommerceLayout from "../layout/ProcenterCommerceLayout";
 
 export const InformacionEmpresarial = () => {
   return (
     <ProcenterCommerceLayout>
-        <Typography>Información empresarial</Typography>
+      <Grid container display={"flex"} justifyContent={"center"} spacing={2}>
+        <Grid item sm={6} xs={12} sx={{ textAlign: "center" }}>
+          <Typography
+            sx={{ fontSize: "30pt", color: "gray", fontWeight: "bold" }}
+          >
+            ¿QUIENES SOMOS?
+          </Typography>
+        </Grid>
+        <Grid item sm={6} xs={12}></Grid>
+      </Grid>
+      <Grid
+        container
+        display={"flex"}
+        justifyContent={"center"}
+        sx={{ backgroundColor: "black" }}
+      >
+        <Grid item sm={6} xs={12} sx={{ textAlign: "center" }}>
+          <Box sx={{ width: "100%", mt: 2, mb: 2 }}>
+            <img id="img4" src="../../../public/Images/LUCKY5.png" />
+          </Box>
+        </Grid>
+        <Grid item sm={6} xs={12}>
+          <Typography sx={{ color: "white", mt: 2, mb: 2 }}>
+            BBLABLABLBALBALBALBALBAL BALBALBALBALABLABLABLABAL
+            BLABALBALBALBALBALBALBALBA BLABLABLABLABLABLABALBALBL
+          </Typography>
+        </Grid>
+      </Grid>
     </ProcenterCommerceLayout>
-  )
-}
+  );
+};
