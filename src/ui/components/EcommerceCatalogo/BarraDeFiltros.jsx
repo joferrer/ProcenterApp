a7516beb55230979a22c73d65db168c2632dfb2f
Jeison Ferrer <jeison120120@hotmail.com>
2023-06-05 21:25:36 -0500

    feact: premerge

--- src/ui/components/EcommerceCatalogo/BarraDeFiltros.jsx
+++ src/ui/components/EcommerceCatalogo/BarraDeFiltros.jsx
@@ -14,6 +14,7 @@ export const BarraDeFiltros = () => {
                     <CollapseFiltro filtro={"Precio"}>
                       <SliderMuiComponent rango={[0,100]}/>
                     </CollapseFiltro>
+                    
             </Grid>
             
         </Grid>
