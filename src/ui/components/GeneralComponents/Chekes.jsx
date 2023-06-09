be6cd8bd50c38a51be7863e6d2be261bfc1c5b70
GUILLERMOGU24 <jguillermogu24@gmail.com>
2023-06-09 17:47:21 -0500

    filtro home listo

--- src/ui/components/GeneralComponents/Chekes.jsx
+++ src/ui/components/GeneralComponents/Chekes.jsx
@@ -3,21 +3,28 @@ import { FormControlLabel, Checkbox, Box } from "@mui/material";
 
 export default function CheckboxList({ marca }) {
   const [selectedMarca, setSelectedMarca] = useState("");
+
   const marcas = ["KIA", "RENAULT", "MERCEDES", "TOYOTA", "BMW"];
 
   const handleCheckboxChange = (event) => {
-    setSelectedMarca(event.target.value);
-    marca(event.target.value);
+    const value = event.target.value;
+    if (selectedMarca === value) {
+      setSelectedMarca("");
+      marca("");
+    } else {
+      setSelectedMarca(value);
+      marca(value);
+    }
   };
 
-  const columns = Math.ceil(marcas.length / 3); // Calcula la cantidad de columnas necesarias
+  const columns = Math.ceil(marcas.length / 3);
 
   return (
-    <Box display={"flex"}>
+    <Box display="flex">
       {Array.from({ length: columns }, (_, colIndex) => (
         <Box key={colIndex} flex={1}>
           {marcas
-            .slice(colIndex * columns, colIndex * columns + columns) // Obtiene las marcas para la columna actual
+            .slice(colIndex * columns, colIndex * columns + columns)
             .map((marca, index) => (
               <FormControlLabel
                 key={index}
