f735aba4a123960ced6f861eb8b085b0cb4e5687
Jeison Ferrer <jeison120120@hotmail.com>
2023-06-16 02:21:07 -0500

    feact: quienes somos

--- src/procentercommerce/pages/InformacionEmpresarial.jsx
+++ src/procentercommerce/pages/InformacionEmpresarial.jsx
@@ -2,7 +2,12 @@ import { Box, Grid, Typography } from "@mui/material";
 import ProcenterCommerceLayout from "../layout/ProcenterCommerceLayout";
 import CarouselComponent from "../../ui/components/GeneralComponents/carousel";
 import img2 from "../../../public/Images/LUCKY5.png";
+import clienteFeliz from "../../../public/Images/ClienteFeliz1.jpeg"
+import { InformacionDispatch } from "../../store/informacion/InformacionDispatch";
 export const InformacionEmpresarial = () => {
+
+  const {informacion} = InformacionDispatch()
+
   return (
     <ProcenterCommerceLayout>
       <Grid container display={"flex"} justifyContent={"center"} spacing={2}>
@@ -19,7 +24,11 @@ export const InformacionEmpresarial = () => {
             }}
           >
             ¿QUIENES SOMOS?
+            
           </Typography>
+          <Typography sx={{  mt: 2, mb: 2 }}>{
+              informacion?.quienesSomos
+            }</Typography>
         </Grid>
         <Grid item sm={6} xs={12}>
           <Box sx={{ p: 2 }}>
@@ -39,12 +48,34 @@ export const InformacionEmpresarial = () => {
           </Box>
         </Grid>
         <Grid item sm={6} xs={12}>
+          <Typography variant="h3"  sx={{ color: "white", mt: 2, mb: 2 }}>Visión</Typography>
           <Typography sx={{ color: "white", mt: 2, mb: 2 }}>
-            BBLABLABLBALBALBALBALBAL BALBALBALBALABLABLABLABAL
-            BLABALBALBALBALBALBALBALBA BLABLABLABLABLABLABALBALBL
+            {informacion?.vision}
           </Typography>
         </Grid>
+        
       </Grid>
+      <Grid
+        container
+        display={"flex"}
+        justifyContent={"center"}
+        
+      >
+        <Grid item sm={6} xs={12}>
+          <Typography variant="h3"  sx={{ ml:2, mt: 2, mb: 2 }}>Misión</Typography>
+          <Typography sx={{ ml:2, mt: 2, mb: 2 }}>
+            {informacion?.mision}
+          </Typography>
+        </Grid>
+        
+        <Grid item sm={6} xs={12} sx={{ textAlign: "center" }}>
+          <Box sx={{ width: "100%", mt: 2, mb: 2 }}>
+            <img style={{width:400, height: 300}} src={clienteFeliz} />
+          </Box>
+        </Grid>
+        
+      </Grid>
+      
     </ProcenterCommerceLayout>
   );
 };
